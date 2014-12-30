40fe308a471455235e706c97acc68e6cd48df4f3
Ryoma ABE <arayahomuhomu@gmail.com>
2014-12-30 21:44:44 +0900

    loginはいらなそうなので削除

--- config/initializers/flickr.rb
+++ config/initializers/flickr.rb
@@ -2,4 +2,4 @@ FlickRaw.api_key = Rails.application.secrets.flickr_api_key
 FlickRaw.shared_secret = Rails.application.secrets.flickr_shared_secret
 flickr.access_token = Rails.application.secrets.flickr_access_token
 flickr.access_secret = Rails.application.secrets.flickr_shared_secret
-flickr.test.login
+
