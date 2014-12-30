c856824b468b1df379e8665dab8fff3cc884568b
Ryoma ABE <arayahomuhomu@gmail.com>
2014-12-31 00:57:54 +0900

    Twitterのハッシュタグから寿司の画像をとってきて表示
    ref #4

--- app/controllers/sushi_controller.rb
+++ app/controllers/sushi_controller.rb
@@ -1,10 +1,31 @@
+require 'twitter_client'
+
 class SushiController < ApplicationController
   def flickr_photo
     photos = flickr.photos.search(text: '寿司', license:"1,2,3,4,5,6")
     @sushi_urls = Array.new
-      photos.each do |photo|
+    photos.each do |photo|
       @sushi_urls.push("http://farm#{photo.farm}.static.flickr.com/#{photo.server}/#{photo.id}_#{photo.secret}_n.jpg")
     end
+  end
 
+  def twitter_photo
+    @sushi_urls_twitter = get_sushi_from_twitter
+  end
+
+  private
+  def get_sushi_from_twitter 
+    photos = Array.new
+    sushi_tweets = TwitterClient.client.search("#寿司").to_h[:statuses]
+    sushi_tweets.each do |tweet|
+      images = tweet[:entities][:media]
+      if images.class == Array
+        images.each do |image|
+          photos.push(image[:media_url])
+        end
+      end
+    end
+    photos
   end
 end
+
