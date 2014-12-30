f06166b3fdb01f08710372a34209c911a16778d2
Ryoma ABE <arayahomuhomu@gmail.com>
2014-12-30 22:09:16 +0900

    flickrから画像を取得して表示できるようになった

--- app/controllers/sushi_controller.rb
+++ app/controllers/sushi_controller.rb
@@ -1,5 +1,10 @@
 class SushiController < ApplicationController
   def flickr_photo
-    @photos = flickr.photos.search(text: '寿司', license:"1,2,3,4,5,6")
+    photos = flickr.photos.search(text: '寿司', license:"1,2,3,4,5,6")
+    @sushi_urls = Array.new
+      photos.each do |photo|
+      @sushi_urls.push("http://farm#{photo.farm}.static.flickr.com/#{photo.server}/#{photo.id}_#{photo.secret}_n.jpg")
+    end
+
   end
 end
