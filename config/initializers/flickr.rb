b651cb374f4ea71b0a87992edaa533eed8c59e66
alitaso345 <alice.maru345@gmail.com>
2014-12-30 20:48:49 +0900

    flickrの認証のためのinitializerを追加

---
+++ config/initializers/flickr.rb
@@ -0,0 +1,5 @@
+FlickRaw.api_key = Rails.application.secrets.flickr_api_key
+FlickRaw.shared_secret = Rails.application.secrets.flickr_shared_secret
+flickr.access_token = Rails.application.secrets.flickr_access_token
+flickr.access_secret = Rails.application.secrets.flickr_shared_secret
+flickr.test.login
