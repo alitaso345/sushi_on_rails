b6252eb743690a85fb19b37f855da37e9a1f9c13
alitaso345 <alice.maru345@gmail.com>
2014-12-31 11:32:49 +0900

    画像を取得するためのロジック
    ref #6

--- app/controllers/sushi_controller.rb
+++ app/controllers/sushi_controller.rb
@@ -11,4 +11,20 @@ class SushiController < ApplicationController
   def tumblr_photo
     @sushi_urls_from_tumblr = get_sushi_from_tumblr()
   end
+
+  private
+  def get_sushi_from_tumblr
+    sushi_photos = []
+    client = Tumblr::Client.new
+    posts = client.tagged("寿司",{})
+    posts.each do |post|
+      if(post["type"] == "photo")
+        post["photos"].each do |potho|
+          sushi_photos.push(potho["alt_sizes"][2]["url"])
+        end
+      end
+    end
+
+    sushi_photos
+  end
 end
