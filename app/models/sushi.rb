78f83d0472834d2c6f28d1ee9e638d59d67176c3
alitaso345 <alice.maru345@gmail.com>
2014-12-31 19:24:52 +0900

    searchメソッドは全てのアプリから寿司画像を取ってくるメソッド
    ref #9

--- app/models/sushi.rb
+++ app/models/sushi.rb
@@ -1,2 +1,24 @@
 class Sushi < ActiveRecord::Base
+  class << self
+    def search
+      results = get_sushi_from_tumblr()
+
+      results
+    end
+
+    def get_sushi_from_tumblr()
+      sushi_photos = []
+      client = Tumblr::Client.new
+      posts = client.tagged("寿司",{})
+      posts.each do |post|
+        if(post["type"] == "photo")
+          post["photos"].each do |photo|
+            sushi_photos.push({:url =>  photo["alt_sizes"][2]["url"], :provider => "tumblr" })
+          end
+        end
+      end
+
+      sushi_photos
+    end
+  end
 end
